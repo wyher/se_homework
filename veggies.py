c3e75fe46b4f86edc425fdeaf2d1c8ce86265cda
hanyu.wang <2547227917@qq.com>
2022-03-21 08:45:01 +0800

    veggies order system

--- veggies.py
+++ veggies.py
@@ -1,2 +1,7 @@
-veggies = ['cabbage', 'carrot', 'spinach', 
-'asparagus','artichoke','lettuce']
+veggies = {'cabbage':12, 'carrot':23, 'spinach':10, 'asparagus':19,'artichoke':67,'lettuce':30}
+
+total = 0
+
+for price in veggies.values():
+	total += price
+print(total)
